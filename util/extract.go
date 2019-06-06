81268152c512cff6908d8a7baec880a26c9459b3
jinwu <jinwei.wu@nokia.com>
2019-06-06 09:43:42 +0800

    change output from file to mysql

--- util/extract.go
+++ util/extract.go
@@ -11,6 +11,10 @@ import (
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
 	"regexp"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"os/signal"
+	"syscall"
 )
 
 type TypeItem struct {
@@ -51,6 +55,12 @@ var (
 	col = Collections{}
 	//cfileloc = "C:\\Users\\jinwu\\go\\src\\jwCmdApp\\cm.yaml"
 	cfileloc = "./cm.yaml"
+	dbDriver   = "mysql"
+	user     = "grafana"
+	password = "12345.Qwert"
+	hostIP   = "127.0.0.1"
+	port     = "3306"
+	database = "forgrafana"
 )
 
 
@@ -62,7 +72,9 @@ func FileExists(path string) bool {
 
 func (r *Resources) TearDown()  {
 	fmt.Println("wait monitor workers start")
+	r.HandleSignal()
 	r.Wg.Wait()
+	os.Remove(r.OpFileloc + strconv.Itoa(os.PathSeparator) + strconv.Itoa(os.Getpid()) + ".pid")
 	fmt.Println("wait monitor workers end\n\nBye bye!")
 }
 
@@ -130,6 +142,31 @@ func (r *Resources) Init() error {
 	//fmt.Printf("%s\n%s\n", res.Nodes, res.Pods)
 }
 
+func (r *Resources) HandleSignal()  {
+
+	// handle system signals
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+
+	f, err := os.OpenFile(r.OpFileloc + strconv.Itoa(os.PathSeparator) + strconv.Itoa(os.Getpid()) + ".pid", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Create file %s failed!\n", f.Name())
+	}
+
+	go func() {
+		for {
+			select {
+			case <- sigs:
+				fmt.Println("Termination signal received! Bye bye!")
+				f.Close()
+				os.Remove(r.OpFileloc + strconv.Itoa(os.PathSeparator) + strconv.Itoa(os.Getpid()) + ".pid")
+				os.Exit(2)
+			}
+		}
+	}()
+
+}
+
 func ReplMB(src *string, regex string, repl string) error {
 	r, err := regexp.Compile(regex)
 	if err != nil {
@@ -157,32 +194,156 @@ func IsEmptyFile(path string) bool {
 	return true
 }
 
+//
+func ConvertCmdOutput(kubeOut []byte, name string, str *string) []string  {
+	var kubeCmd *exec.Cmd
+	if strings.HasPrefix(name, "isdk-cluster") {
+		kubeCmd = exec.Command("kubectl", "top", "node", name)
+	} else {
+		kubeCmd = exec.Command("kubectl", "top", "pod", name)
+		//fmt.Printf("kubectl top pod %s\n", name)
+	}
+
+	kubeOut, err := kubeCmd.Output()
+	if err != nil {
+		panic("cmd failed: " + err.Error())
+	}
+
+	regexp.Compile("[ ]+")
+	*str = strings.Split(string(kubeOut), "\n")[1]
+	// replace multiple blanks to one
+	ReplMB(str, "[ ]+", " ")
+	// replace one blank to a comma
+	ReplMB(str, " ", ",")
+	// drop the last comma of the string
+	tmp := *str
+	*str = tmp[:len(tmp) - 1]
+
+	// split the string on comma, and replace the unit of each field
+	tmpstrs := strings.Split(*str, ",")
+	//var joinbackstr string
+	if strings.HasPrefix(name, "isdk-cluster") {  // node
+		tmpstrs[1] = tmpstrs[1][:len(tmpstrs[1]) - 1]
+		tmpstrs[2] = tmpstrs[2][:len(tmpstrs[2]) - 1]
+		tmpstrs[3] = tmpstrs[3][:len(tmpstrs[3]) - 2]
+		tmpstrs[4] = tmpstrs[4][:len(tmpstrs[4]) - 1]
+	} else {   // pod
+		tmpstrs[1] = tmpstrs[1][:len(tmpstrs[1]) - 1]
+		tmpstrs[2] = tmpstrs[2][:len(tmpstrs[2]) - 2]
+	}
+	//joinbackstr = strings.Join(tmpstrs, ",")
+	//*str = joinbackstr
+	//fmt.Println(str)
+	return tmpstrs
+}
+
+func DialMysql(user, password, hostIP, port, database string) (error, *sql.DB)  {
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, hostIP, port, database)
+	db, err := sql.Open(dbDriver, dataSource)
+	//db, err := sql.Open(DBDriver, User + ":" + Password + "@tcp(" + HostIP + ":" + PORT + ")/" + Database)
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}  else {
+		fmt.Println("DialMysql successfully")
+	}
+	return db.Ping(), db
+}
+
+func InsertData(db *sql.DB, vals []string)  {
+	var name, cpu, cpuPercent, memery, memeryPercent string
+	if strings.HasPrefix(vals[0], "isdk-cluster") { // node
+		name          = vals[0]
+		cpu           = vals[1]
+		cpuPercent    = vals[2]
+		memery        = vals[3]
+		memeryPercent = vals[4]
+	} else {  // pod
+		name          = vals[0]
+		cpu           = vals[1]
+		memery        = vals[2]
+	}
+
+	timeStamp := time.Now().UTC().Add(4 * 60 * time.Minute + 10 * time.Minute).Format(time.RFC3339)
+
+
+	if strings.HasPrefix(vals[0], "isdk-cluster") {  // node
+		insForm, err := db.Prepare("INSERT INTO MonitorNode(Date, Name, CPU_m_cores, CPU_percentage, MEMORY_Mi_bytes, MEMORY_percentage) VALUES(?,?,?,?,?,?)")
+		if err != nil {
+			panic(err.Error())
+		} else {
+			//fmt.Println("db sql prepare successfully.")
+		}
+
+		_, err = insForm.Exec(timeStamp[:19], name, cpu, cpuPercent, memery, memeryPercent)
+		//_, err = insForm.Exec("2019-06-04T12:25:49", "isdk-cluster-control-03", 580, 20, 2586, 90)
+		if err != nil {
+			panic(err.Error())
+		} else {
+			//fmt.Println("db sql execution successfully.")
+		}
+		fmt.Println(fmt.Sprintf("%v | %s, %s, %s, %s, %s", timeStamp[:19], name, cpu, cpuPercent, memery, memeryPercent))
+	} else {   // pod
+		insForm, err := db.Prepare("INSERT INTO MonitorPod(Date, Name, CPU_m_cores, MEMORY_Mi_bytes) VALUES(?,?,?,?)")
+		if err != nil {
+			panic(err.Error())
+		} else {
+			//fmt.Println("db sql prepare successfully.")
+		}
+
+
+		_, err = insForm.Exec(timeStamp[:19], name, cpu, memery)
+		//_, err = insForm.Exec("2019-06-04T12:25:49", "isdk-cluster-control-03", 580, 20, 2586, 90)
+		if err != nil {
+			panic(err.Error())
+		} else {
+			//fmt.Println("db sql execution successfully.")
+		}
+		fmt.Println(fmt.Sprintf("%v | %s, %s, %s", timeStamp[:19], name, cpu, memery))
+	}
+}
+
 func (r *Resources) CmdOutput(list []string, cmd string)  {
+
+	err, db := DialMysql(user, password, hostIP, port, database)
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}  else {
+		//fmt.Println("Database created successfully")
+	}
+
+	// use given database
+	_, err = db.Exec("USE forgrafana")
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("DB 'forgrafana' selected successfully!")
+	}
+
 	for i, v := range list {
 		//r.Wg.Add(1)
 		go func(id int, name string) {
-			var outpfExist bool
-			var outputFile string = r.OpFileloc + name + ".csv"
-
+			var outpfExist bool = true
 			t := time.NewTicker(r.Interval)
 
-			if IsEmptyFile(outputFile) {
-				outpfExist = false
-			} else {
-				outpfExist = true
-			}
-
-			f, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				fmt.Printf("Create file %s failed!\n", f.Name())
-			}
 			tm := time.NewTimer(r.Duration)
-
 			if !outpfExist {   // only not exist, then add title
 				if strings.HasPrefix(name, "isdk-cluster") {  //node
-					f.WriteString("TIME,NAME,CPU(cores),CPU%,MEMORY(bytes),MEMORY%\n")
+					var outputFile string = r.OpFileloc + name + ".csv"
+
+					if IsEmptyFile(outputFile) {
+						outpfExist = false
+					} else {
+						outpfExist = true
+					}
+
+					f, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+					if err != nil {
+						fmt.Printf("Create file %s failed!\n", f.Name())
+					}
+					//f.WriteString("TIME,NAME,CPU(m cores),CPU%,MEMORY(Mi bytes),MEMORY%\n")
+
 				} else {  // pod
-					f.WriteString("TIME,NAME,CPU(cores),MEMORY(bytes)\n")
+					//f.WriteString("TIME,NAME,CPU(m cores),MEMORY(Mi bytes)\n")
 				}
 			}
 
@@ -190,38 +351,28 @@ func (r *Resources) CmdOutput(list []string, cmd string)  {
 				select {
 				case <-t.C:
 					var kubeOut []byte
+					var str string
+					var vals []string
 					//var err error
 					if r.Debug {
-						kubeOut = []byte(time.Now().Format(time.RFC3339Nano))
+						kubeOut = []byte(time.Now().Format(time.RFC3339))
+						str = string(kubeOut)
 					} else {
-						var kubeCmd *exec.Cmd
-						if strings.HasPrefix(name, "isdk-cluster") {
-							kubeCmd = exec.Command("kubectl", "top", "node", name)
-						} else {
-							kubeCmd = exec.Command("kubectl", "top", "pod", name)
-							//fmt.Printf("kubectl top pod %s\n", name)
-						}
-
-						kubeOut, err = kubeCmd.Output()
-						if err != nil {
-							fmt.Printf("cmd failed: %s!\n", err)
-						}
+						vals = ConvertCmdOutput(kubeOut, name, &str)
 					}
 
-					regexp.Compile("[ ]+")
-					str := strings.Split(string(kubeOut), "\n")[1]
-					ReplMB(&str, "[ ]+", " ")
-					ReplMB(&str, " ", ",")
-					str = str[:len(str) -1]
+					now := time.Now().Format(time.RFC3339)
+					now = now[:19]
 
-					now := time.Now().Format(time.RFC3339Nano)
-					f.WriteString(now + "," + string(str) + "\n")
+					//fmt.Println(fmt.Sprintf("%s, %s\n", now, strings.Join(vals, ",")))
 
-					fmt.Printf("%55v | #%d worker gets top info\n", time.Now(), id)
+					InsertData(db, vals)
+					//f.WriteString(now + "," + str + "\n")
+					//fmt.Printf("%55v | #%d worker gets top info\n", time.Now(), id)
 
 				case <- tm.C:
 					fmt.Printf("time is up, #%d monitor worker ends!\n", id )
-					f.Close()
+					//f.Close()
 					r.Wg.Done()
 					return
 				}
