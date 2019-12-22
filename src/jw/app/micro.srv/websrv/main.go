512344198b0eda1dde348db38426045b779094a6
laodano1 <laodano1@outlook.com>
2019-12-22 12:53:36 +0800

    integrate gin web server to go micro web framework

--- src/jw/app/micro.srv/websrv/main.go
+++ src/jw/app/micro.srv/websrv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/web"
 	"log"
 	"net/http"
@@ -25,6 +26,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world"))
 }
 
+func homepage(c *gin.Context) {
+	c.String(http.StatusOK, "hellllll worlllld")
+}
+
+func aboutpage(c *gin.Context) {
+	c.String(http.StatusOK, "about page")
+}
+
 func main() {
 
 	webSrv := web.NewService(
@@ -33,8 +42,23 @@ func main() {
 			o.Address = ":9999"
 		},
 	)
-	webSrv.HandleFunc("/", index)
 
+	router := gin.Default()
+	router.HandleMethodNotAllowed = true
+	router.NoMethod(func(c *gin.Context) {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{"result": false, "error": "Method Not Allowed"})
+		return
+	})
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"result": false, "error": "Endpoint Not Found"})
+		return
+	})
+
+	router.GET("/", homepage)
+	router.GET("/about", aboutpage)
+
+	webSrv.Handle("/", router)
 	if err := webSrv.Run(); err != nil {
 		log.Fatal("go micro web server error: ", err)
 	}
