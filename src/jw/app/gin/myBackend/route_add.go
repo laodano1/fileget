1eaded65d3631e6fa0a912f6257ddc6d6fe50773
jinwei <laoda@outlook.com>
2020-04-26 16:52:35 +0800

    update

--- src/jw/app/gin/myBackend/route_add.go
+++ src/jw/app/gin/myBackend/route_add.go
@@ -27,78 +27,6 @@ func homepage(c *gin.Context) {
 	c.Render(http.StatusOK, rd)
 }
 
-func buttons(c *gin.Context) {
-	fullPth := fmt.Sprintf("%v%ctemplate%clyear_ui_buttons.html", exeAbsPath, os.PathSeparator, os.PathSeparator)
-	//lg.Debugf("button tmpl: %v", fullPth)
-	tp := template.Must(template.New("button").ParseFiles(fullPth))
-	//
-	dt := &gin.H{
-		"title" : "buttons",
-	}
-
-	rd := render.HTML{
-		Template: tp,
-		Name:     "button",
-		Data:     dt,
-	}
-
-	c.Render(http.StatusOK, rd)
-}
-
-func belements(c *gin.Context) {
-	fullPth := fmt.Sprintf("%v%ctemplate%clyear_forms_elements.html", exeAbsPath, os.PathSeparator, os.PathSeparator)
-	//lg.Debugf("button tmpl: %v", fullPth)
-	tp := template.Must(template.New("elements").ParseFiles(fullPth))
-	//
-	dt := &gin.H{
-		"title" : "elements",
-	}
-
-	rd := render.HTML{
-		Template: tp,
-		Name:     "elements",
-		Data:     dt,
-	}
-
-	c.Render(http.StatusOK, rd)
-}
-
-func examples(c *gin.Context) {
-	fullPth := fmt.Sprintf("%v%ctemplate%clyear_pages_doc.html", exeAbsPath, os.PathSeparator, os.PathSeparator)
-	//lg.Debugf("button tmpl: %v", fullPth)
-	tp := template.Must(template.New("doclist").ParseFiles(fullPth))
-	//
-	dt := &gin.H{
-		"title" : "doc list",
-	}
-
-	rd := render.HTML{
-		Template: tp,
-		Name:     "doclist",
-		Data:     dt,
-	}
-
-	c.Render(http.StatusOK, rd)
-}
-
-func datechs(c *gin.Context) {
-	fullPth := fmt.Sprintf("%v%ctemplate%clyear_js_datepicker.html", exeAbsPath, os.PathSeparator, os.PathSeparator)
-	//lg.Debugf("button tmpl: %v", fullPth)
-	tp := template.Must(template.New("dtch").ParseFiles(fullPth))
-	//
-	dt := &gin.H{
-		"title" : "date choose",
-	}
-
-	rd := render.HTML{
-		Template: tp,
-		Name:     "dtch",
-		Data:     dt,
-	}
-
-	c.Render(http.StatusOK, rd)
-}
-
 func (m *myBackend)  staticHandle(group string)  {
 	m.e.Static(fmt.Sprintf("%v/css", group),    fmt.Sprintf("%v%ctemplate%ccss", exeAbsPath, os.PathSeparator, os.PathSeparator))
 	m.e.Static(fmt.Sprintf("%v/js", group),     fmt.Sprintf("%v%ctemplate%cjs", exeAbsPath, os.PathSeparator, os.PathSeparator))
@@ -153,7 +81,10 @@ func (m *myBackend) addRoutes() {
 
 	formserg := m.e.Group("/forms")
 	m.staticHandle("/forms")
-	formserg.Handle("GET", "/elements", belements)
+	formserg.Handle("GET", "/elements", m.generalHandle("lyear_forms_elements.html"))
+	formserg.Handle("GET", "/radio", m.generalHandle("lyear_forms_radio.html"))
+	formserg.Handle("GET", "/switch", m.generalHandle("lyear_forms_switch.html"))
+	formserg.Handle("GET", "/checkbox", m.generalHandle("lyear_forms_checkbox.html"))
 
 	exapserg := m.e.Group("/examples")
 	m.staticHandle("/examples")
