76178244523214f2d5e131d955d729349ef5db6c
laodano1 <281296869@qq.com>
2020-04-28 21:33:54 +0800

    finish all template

---
+++ src/jw/app/gin/condition/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/davyxu/golog"
+)
+
+const (
+	b = 0 - 1
+)
+
+type la struct{
+	a string
+}
+
+type lai interface {
+
+}
+
+func aaa(i lai)  {
+	//lg.Debugf("i: %t", i.(type))
+	switch ia := i.(type) {
+	case la:
+		lg.Debugf("la: %v", ia)
+	}
+}
+
+var lg = golog.New("condition")
+
+func main() {
+
+	a := 0 == 0
+
+	lg.Debugf("a: %v, b: %v", a, b)
+
+	t := 1 << 11  //
+	lg.Debugf("t: %d", t)
+
+	latmp := la{a: "test"}
+	aaa(latmp)
+
+
+
+	//net.Listen()
+
+
+}
