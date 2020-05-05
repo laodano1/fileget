c07b16c2c0c25707ab4f312ec3d960ff849e620e
laodano1 <281296869@qq.com>
2020-05-05 09:52:02 +0800

    update

---
+++ src/jw/app/defpack/algorihtm/quicksort/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := []int{11, 12, 42, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	list = quickSort(list)
+	fmt.Printf("fastsort list: %v\n", list)
+}
+
+func quickSort(arr []int) []int {
+	return _quickSort(arr, 0, len(arr)-1)
+}
+
+func _quickSort(arr []int, left, right int) []int {
+	if left < right {
+		partitionIndex := partition(arr, left, right)
+		_quickSort(arr, left, partitionIndex-1)
+		_quickSort(arr, partitionIndex+1, right)
+	}
+	return arr
+}
+
+func partition(arr []int, left, right int) int {
+	pivot := left
+	index := pivot + 1
+
+	for i := index; i <= right; i++ {
+		if arr[i] < arr[pivot] {
+			swap(arr, i, index)
+			index += 1
+		}
+	}
+	swap(arr, pivot, index-1)
+	return index - 1
+}
+
+func swap(arr []int, i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
