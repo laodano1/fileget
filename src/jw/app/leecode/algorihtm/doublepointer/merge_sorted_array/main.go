70453996aeafd9248fb702ebef1c0b97a36a5346
jinwei <laoda@outlook.com>
2020-05-26 18:38:08 +0800

    update

---
+++ src/jw/app/leecode/algorihtm/doublepointer/merge_sorted_array/main.go
@@ -0,0 +1,30 @@
+package main
+
+// m, n分别为两个数组的长度
+// 需要从尾开始遍历，否则在 nums1 上归并得到的值会覆盖还未进行归并比较的值。
+func merge_sorted_arrays(nums1, nums2 []int, m, n int) (output []int) {
+	i := m - 1; j := n -1
+	k := m + n - 1
+
+	for i >= 0 || j >= 0 {
+		if i < 0 {
+			nums1[k] = nums2[j]
+			k--; j--
+		} else if j < 0 {
+
+		} else if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			k--; i--
+		} else {
+			nums1[k] = nums2[j]
+			k--; j--
+		}
+	}
+
+	return
+}
+
+
+func main() {
+
+}
