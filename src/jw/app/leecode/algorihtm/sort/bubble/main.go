fb783eda7b947241f2277b04e1350513a90d554f
laodano1 <281296869@qq.com>
2020-05-28 22:16:41 +0800

    update bubble sort

--- src/jw/app/leecode/algorihtm/sort/bubble/main.go
+++ src/jw/app/leecode/algorihtm/sort/bubble/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fileget/util"
+)
 
 func bubble(list []int) []int {
 
@@ -9,17 +11,33 @@ func bubble(list []int) []int {
 			if list[i] > list[j] {
 				list[j], list[i] = list[i], list[j]
 			}
+			util.Lg.Debugf("i: %v, %v", i, list)
 		}
+		//util.Lg.Debugf("i: %v, %v", i, list)
+		util.Lg.Debugf("  ")
 	}
-
-
 	return list
 }
 
+func bubble2(input []int) (output []int)  {
+	//for i, _ := range input {
+	for i := 0; i < len(input) - 1; i++ {
+		for j := 1; j < len(input) - i; j++ {
+			if input[j-1] > input[j] {
+				input[j-1], input[j] = input[j], input[j-1]
+			}
+			util.Lg.Debugf("j: %v, %v", j, input)
+		}
+		util.Lg.Debugf("  ")
+	}
+	output = input
+	return
+}
 
 func main() {
-	//list := []int{9, 0, 7, 6, 5, 4, 3, 2, 1, 8}
-	list := []int{6, 0, 7, 9, 5, 4, 3, 2, 1, 8}
-	list = bubble(list)
-	fmt.Printf("list: %v\n", list)
+	list := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	//list := []int{6, 0, 7, 9, 5, 4, 3, 2, 1, 8}
+	//list = bubble(list)
+	list = bubble2(list)
+	util.Lg.Debugf("list: %v\n", list)
 }
