0182eaaaec19866fcd88e8974e7f77f4689b7736
laodano1 <281296869@qq.com>
2020-05-28 23:03:46 +0800

    add quick sort

--- src/jw/app/leecode/algorihtm/sort/fastsort/main.go
+++ src/jw/app/leecode/algorihtm/sort/fastsort/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fileget/util"
+	"math/rand"
+)
 
 func fastsort(list []int) []int {
 
@@ -8,9 +11,38 @@ func fastsort(list []int) []int {
 	return list
 }
 
+func quicksort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	quicksort(a[:left])
+	quicksort(a[left+1:])
+
+	return a
+}
+
 
 func main() {
-	list := []int{9, 0, 7, 6, 5, 4, 3, 2, 1, 8}
-	list = fastsort(list)
-	fmt.Printf("fastsort list: %v\n", list)
+	list := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	list = quicksort(list)
+	util.Lg.Debugf("output: %v\n", list)
+
+	a, b, c := 1, 2, 3
+	util.Lg.Debugf("%v, %v, %v", a, b, c)
 }
