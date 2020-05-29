569c28172404c1f0f7956afc830fc20d3be8087f
jinwei <laoda@outlook.com>
2020-05-29 10:37:33 +0800

    add benchmark test for quick sort

--- src/jw/app/leecode/algorihtm/sort/fastsort/main.go
+++ src/jw/app/leecode/algorihtm/sort/fastsort/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 // trade space for time
-func fastsort(input []int) (output []int) {
+func Fastsort(input []int) (output []int) {
 	//l := 0; 	r := len(input) - 1
 	if len(input) <= 1 { return input}
 
@@ -24,28 +24,28 @@ func fastsort(input []int) (output []int) {
 			tmpR = append(tmpR, input[i])
 		}
 	}
-	util.Lg.Debugf("tmpL: %v", tmpL)
-	util.Lg.Debugf("tmpR: %v", tmpR)
-	util.Lg.Debugf("")
+	//util.Lg.Debugf("tmpL: %v", tmpL)
+	//util.Lg.Debugf("tmpR: %v", tmpR)
+	//util.Lg.Debugf("")
 
-	output = append(output, fastsort(tmpL)...)
+	output = append(output, Fastsort(tmpL)...)
 	output = append(output, input[pivot])
-	output = append(output, fastsort(tmpR)...)
+	output = append(output, Fastsort(tmpR)...)
 
 	return
 }
 
 //
-func quicksort1(a []int) []int {
+func Quicksort1(a []int) []int {
 	if len(a) < 2 { return a }
 
 	left, right := 0, len(a)-1
 
 	//pivot := rand.Int() % len(a)
 	pivot := len(a)/2
-	util.Lg.Debugf("1. input: %v, pivot: %v", a, pivot)
+	//util.Lg.Debugf("1. input: %v, pivot: %v", a, pivot)
 	a[pivot], a[right] = a[right], a[pivot]
-	util.Lg.Debugf("2. input: %v", a)
+	//util.Lg.Debugf("2. input: %v", a)
 	for i, _ := range a {
 		if i == right {continue}
 		if a[i] < a[right] {
@@ -53,12 +53,12 @@ func quicksort1(a []int) []int {
 			left++
 		}
 	}
-	util.Lg.Debugf("3. input: %v, left: %v", a, left)
+	//util.Lg.Debugf("3. input: %v, left: %v", a, left)
 	a[left], a[right] = a[right], a[left]
-	util.Lg.Debugf("4. input: %v", a)
-	util.Lg.Debugf("")
-	quicksort1(a[:left])
-	quicksort1(a[left+1:])
+	//util.Lg.Debugf("4. input: %v", a)
+	//util.Lg.Debugf("")
+	Quicksort1(a[:left])
+	Quicksort1(a[left+1:])
 
 	return a
 }
@@ -66,8 +66,8 @@ func quicksort1(a []int) []int {
 
 func main() {
 	list := []int{11, 88, 77, 66, 9, 34, 7, 6, 5, 24, 3, 2, 1, 0}
-	//list = quicksort1(list)
-	list = fastsort(list)
+	//list = Quicksort1(list)
+	list = Fastsort(list)
 	util.Lg.Debugf("output: %v\n", list)
 
 	//a, b, c := 1, 2, 3
