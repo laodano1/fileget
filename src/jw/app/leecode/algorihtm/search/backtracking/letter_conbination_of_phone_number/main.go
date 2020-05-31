7dc1b39d72a682b6fa15f3b2913d35245186ebf3
laodano1 <281296869@qq.com>
2020-05-31 22:54:27 +0800

    add ip address restore

---
+++ src/jw/app/leecode/algorihtm/search/backtracking/letter_conbination_of_phone_number/main.go
@@ -0,0 +1,15 @@
+package main
+
+func letter_conbination_of_phone_number() {
+
+}
+
+func main() {
+	//util.Lg.Debugf("output: %d", '0')
+	//util.Lg.Debugf("output: %d", '9')
+	//util.Lg.Debugf("output: %d", 'A')
+	//util.Lg.Debugf("output: %d", 'Z')
+	//util.Lg.Debugf("output: %d", 'a')
+	//util.Lg.Debugf("output: %d", 'z')
+	//util.Lg.Debugf("output: %d", '3' - '0')
+}
