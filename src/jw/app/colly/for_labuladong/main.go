20f14ad819765f7213a900b4ef638ec6b9d3a0e7
jw <laodano1@outlook.com>
2020-08-07 21:33:01 +0800

    update

--- src/jw/app/colly/for_labuladong/main.go
+++ src/jw/app/colly/for_labuladong/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"fileget/util"
 	"github.com/gocolly/colly/v2"
-	"regexp"
+	"net"
 	"strings"
 )
 
 var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
 
-func main() {
+var linksMap = make(map[string]string)
 
+func Traverse(urlStr string, c *colly.Collector) {
 	c := colly.NewCollector(
 		colly.CacheDir("./labuladong"),
 		colly.UserAgent(UserAgent),
@@ -24,27 +24,38 @@ func main() {
 	c.OnHTML("html", func(e *colly.HTMLElement) {
 		title := e.DOM.Children().Filter("head").Children().Filter("title").Text()
 		title = strings.ReplaceAll(title, " ", "")
-		//if err := e.Response.Save(fmt.Sprintf("%v.html", title)); err != nil {
-		//	util.Lg.Debugf("save error: %v", err)
-		//}
 
-		//util.Lg.Debugf("%v", e.Text)
-
-		bts := bytes.ReplaceAll(e.Response.Body, []byte("href=\"/algo"), []byte("href=\"https://labuladong.gitbook.io/algo"))
-		//
+		//bts := bytes.ReplaceAll(e.Response.Body, []byte("href=\"/algo"), []byte("href=\"https://labuladong.gitbook.io/algo"))
 		//err := ioutil.WriteFile(fmt.Sprintf("%v.html", title), bts, 0777)
 		//if err != nil {
 		//	util.Lg.Debugf("save error: %v", err)
 		//}
-		//util.Lg.Debugf("%v", string(bts))
 
-		reg, _ := regexp.Compile("href=\"https://labuladong.gitbook.io/.*\"")
-		all := reg.FindAllStringSubmatch(string(bts), -1)
-		//hpl := reg.FindStringSubmatch()
-		util.Lg.Debugf("%v", all)
+		e.ForEach("a", func(i int, e *colly.HTMLElement) {
+
+			if !strings.HasPrefix(e.Attr("href"), "http") {
+				txt := strings.TrimLeft(e.Text, " ")
+				txt = strings.TrimRight(txt, " ")
+				linksMap[strings.Replace(e.Attr("href"), "/algo", "https://labuladong.gitbook.io/algo", 1)] = txt
+			}
+			//util.Lg.Debugf("%v, %v", e.Text, e.Attr("href"))
+		})
+
+		for k, v := range linksMap {
+			util.Lg.Debugf("%-16v, %v", v, k)
+		}
 
 	})
 
-	c.Visit("https://labuladong.gitbook.io/algo/")
+	c.Visit(urlStr)
+}
+
+func main() {
+	urlStr := "https://labuladong.gitbook.io/algo/"
+	c := colly.NewCollector(
+		colly.CacheDir("./labuladong"),
+		colly.UserAgent(UserAgent),
+	)
+	Traverse(urlStr, c)
 
 }
